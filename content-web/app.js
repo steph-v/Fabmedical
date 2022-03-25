b6b64e0def18c554a64a1a02f312530113b7cd06
Stephanie Venkitachalam <steph.venkit@gmail.com>
2022-03-25 04:20:59 +0000

    Add Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("9f434035-3d90-4359-bbc4-fada42925362");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
